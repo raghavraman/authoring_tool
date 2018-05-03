534efbf9d690ac904d7557c582e3d9469ea549b1
Raghav Raman <rghv.raman@gmail.com>
2018-05-03 15:43:52 -0400

    Update editSetOrder.js

--- editSetOrder.js
+++ editSetOrder.js
@@ -1,6 +1,5 @@
-function editSetOrder(req, connection, cb) {
-    var activityId = req.body.activityId
-    let sql = "select id,title,activityId from challenge_authoring where activityId = ? order by position";
+function editSetOrder(req,activityId, connection, cb) {
+    let sql = "select id,title,type,activityId,language,position from challenge_authoring where activityId = ? order by position";
     connection.query(sql, [activityId], function (err, res) {
         if (err) {
             cb({
@@ -8,11 +7,26 @@ function editSetOrder(req, connection, cb) {
                 message: "Error in getting order of activities"
             })
         } else {
-            cb({
+            if (res.length === 0) {
+                let response = {
+                    code: 204,
+                    message: "No examples created by the user yet",
+                    data: res
+                }
+                return cb(response)
+            }
+            let obj = {}
+            for(let i in res) {
+                if(!obj[res[i].type]){
+                    obj[res[i].type] = []
+                }
+                obj[res[i].type].push(res[i])
+            }
+            let response = {
                 code: 200,
-                data: res
-            })
-            console.log(res);
+                data: obj
+            }
+            return cb(response)
         }
     })
 }
